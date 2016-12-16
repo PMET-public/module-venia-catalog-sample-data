06e9359565791d13b490e2313c94e2fd5e1f931d
Jeffrey Britts <jbritts@magento.com>
2016-12-16 09:43:45 -0500

    first commit

---
+++ Model/Product.php
@@ -0,0 +1,130 @@
+<?php
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace MagentoEse\VeniaCatalogSampleData\Model;
+
+use Magento\Framework\Setup\SampleData\Context as SampleDataContext;
+
+/**
+ * Class Product
+ *
+ * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
+ */
+class Product
+{
+    /**
+     * @var \Magento\Framework\Setup\SampleData\FixtureManager
+     */
+    protected $fixtureManager;
+
+    /**
+     * @var string
+     */
+    protected $productType = \Magento\Catalog\Model\Product\Type::TYPE_SIMPLE;
+
+    /**
+     * @var int
+     */
+    protected $attributeSetId;
+
+    /**
+     * @var \Magento\Catalog\Model\Config
+     */
+    protected $catalogConfig;
+
+    /**
+     * @var Product\Converter
+     */
+    protected $converter;
+
+    /**
+     * @var \Magento\Framework\File\Csv
+     */
+    protected $csvReader;
+
+    /**
+     * @var Product\Gallery
+     */
+    protected $gallery;
+
+    /**
+     * @var \Magento\Store\Model\StoreManagerInterface
+     */
+    protected $storeManager;
+
+    /**
+     * @var \Magento\Eav\Model\Config
+     */
+    protected $eavConfig;
+
+    /**
+     * Product constructor.
+     * @param SampleDataContext $sampleDataContext
+     * @param Product\Converter $converter
+     * @param Product\Gallery $gallery
+     * @param \Magento\Store\Model\StoreManagerInterface $storeManager
+     * @param \Magento\Eav\Model\Config $eavConfig
+     */
+
+    protected $objectManager;
+    public function __construct(
+        SampleDataContext $sampleDataContext,
+        \MagentoEse\VeniaCatalogSampleData\Model\Product\Converter $converter,
+        \Magento\Store\Model\StoreManagerInterface $storeManager,
+        \Magento\Eav\Model\Config $eavConfig,
+        \Magento\Framework\ObjectManagerInterface $objectManager
+    ) {
+        $this->fixtureManager = $sampleDataContext->getFixtureManager();
+        $this->converter = $converter;
+        $this->csvReader = $sampleDataContext->getCsvReader();
+        $this->storeManager = $storeManager;
+        $this->eavConfig = $eavConfig;
+        $this->objectManager=$objectManager;
+    }
+
+    /**
+     * @param array $productFixtures
+     * @param array $galleryFixtures
+     * @throws \Exception
+     */
+    public function install(array $productFixtures)
+    {
+        $this->eavConfig->clear();
+
+        foreach ($productFixtures as $fileName) {
+            $fileName = $this->fixtureManager->getFixture($fileName);
+            if (!file_exists($fileName)) {
+                continue;
+            }
+
+            $rows = $this->csvReader->getData($fileName);
+            $header = array_shift($rows);
+
+            foreach ($rows as $row) {
+                $_productsArray[] = array_combine($header, $row);
+            }
+            $this->importerModel = $this->objectManager->create('FireGento\FastSimpleImport2\Model\Importer');
+            $this->importerModel->setImportImagesFileDir('pub/media/catalog/product');
+            $this->importerModel->setValidationStrategy('validation-skip-errors');
+            try {
+                $this->importerModel->processImport($_productsArray);
+            } catch (\Exception $e) {
+                print_r($e->getMessage());
+            }
+
+            print_r($this->importerModel->getLogTrace());
+            print_r($this->importerModel->getErrorMessages());
+            unset ($_productsArray);
+        }
+
+    }
+
+    public function setFixtures(array $fixtures)
+    {
+        $this->fixtures = $fixtures;
+        return $this;
+    }
+
+}
---
+++ Setup/Installer.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace MagentoEse\VeniaCatalogSampleData\Setup;
+
+use Magento\Framework\Setup;
+
+class Installer implements Setup\SampleData\InstallerInterface
+{
+    /**
+     * Setup class for category
+     *
+     * @var \MagentoEse\VeniaCatalogSampleData\Model\Category
+     */
+    protected $categorySetup;
+
+    /**
+     * Setup class for product attributes
+     *
+     * @var \MagentoEse\VeniaCatalogSampleData\Model\Attribute
+     */
+    protected $attributeSetup;
+
+    /**
+     * Setup class for products
+     *
+     * @var \MagentoEse\VeniaCatalogSampleData\Model\Product
+     */
+    protected $productSetup;
+
+    /**
+     * @param \MagentoEse\VeniaCatalogSampleData\Model\Category $categorySetup
+     * @param \MagentoEse\VeniaCatalogSampleData\Model\Attribute $attributeSetup
+     * @param \MagentoEse\VeniaCatalogSampleData\Model\Product $productSetup
+     */
+
+
+    public function __construct(
+        \MagentoEse\VeniaCatalogSampleData\Model\Category $categorySetup,
+        \MagentoEse\VeniaCatalogSampleData\Model\Attribute $attributeSetup,
+        \MagentoEse\VeniaCatalogSampleData\Model\Product $productSetup
+    ) {
+        $this->categorySetup = $categorySetup;
+        $this->attributeSetup = $attributeSetup;
+        $this->productSetup = $productSetup;
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function install()
+    {
+        //$this->attributeSetup->install(['MagentoEse_VeniaCatalogSampleData::fixtures/attributes.csv']);
+        $this->categorySetup->install(['MagentoEse_VeniaCatalogSampleData::fixtures/categories.csv']);
+        //$this->productSetup->install(['MagentoEse_VeniaCatalogSampleData::fixtures/milwaukee_products_1.csv']);
+    }
+}
