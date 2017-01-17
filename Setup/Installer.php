1f937de69ce7fffa2c4dad23ea9b4ead33ce2004
Jeffrey Britts <jbritts@magento.com>
2017-01-17 16:34:13 -0500

    added swatching on size and color

--- Setup/Installer.php
+++ Setup/Installer.php
@@ -31,20 +31,30 @@ class Installer implements Setup\SampleData\InstallerInterface
     protected $productSetup;
 
     /**
+     * Convert fashion_color and fashion_size attribute to swatches
+     *
+     * @var \MagentoEse\VeniaCatalogSampleData\Model\Swatches
+     */
+    protected $swatchesSetup;
+
+    /**
      * @param \MagentoEse\VeniaCatalogSampleData\Model\Category $categorySetup
      * @param \MagentoEse\VeniaCatalogSampleData\Model\Attribute $attributeSetup
      * @param \MagentoEse\VeniaCatalogSampleData\Model\Product $productSetup
+     *  * @param \MagentoEse\VeniaCatalogSampleData\Model\Swatches $swatchesSetup
      */
 
 
     public function __construct(
         \MagentoEse\VeniaCatalogSampleData\Model\Category $categorySetup,
         \MagentoEse\VeniaCatalogSampleData\Model\Attribute $attributeSetup,
-        \MagentoEse\VeniaCatalogSampleData\Model\Product $productSetup
+        \MagentoEse\VeniaCatalogSampleData\Model\Product $productSetup,
+        \MagentoEse\VeniaCatalogSampleData\Model\Swatches $swatchesSetup
     ) {
         $this->categorySetup = $categorySetup;
         $this->attributeSetup = $attributeSetup;
         $this->productSetup = $productSetup;
+        $this->swatchesSetup = $swatchesSetup;
 
     }
 
@@ -54,6 +64,7 @@ class Installer implements Setup\SampleData\InstallerInterface
     public function install()
     {
         $this->attributeSetup->install(['MagentoEse_VeniaCatalogSampleData::fixtures/attributes.csv']);
+        $this->swatchesSetup->install();
         $this->categorySetup->install(['MagentoEse_VeniaCatalogSampleData::fixtures/categories.csv']);
         //$this->productSetup->install(['MagentoEse_VeniaCatalogSampleData::fixtures/milwaukee_products_1.csv']);
     }
